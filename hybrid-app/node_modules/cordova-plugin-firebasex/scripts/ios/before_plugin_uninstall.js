d43c9e684ebed117b30d2e72e607cd70c7e2aada
unknown <miroslav.nedelchev@dentacoin.com>
2021-01-08 11:06:36 +0200

    delete push notifications config files and hybrid-app node_modules

--- hybrid-app/node_modules/cordova-plugin-firebasex/scripts/ios/before_plugin_uninstall.js
+++
@@ -1,9 +0,0 @@
-var helper = require("./helper");
-
-module.exports = function(context) {
-
-    // Remove the build script that was added when the plugin was installed.
-    helper.getXcodeProjectPath(function(xcodeProjectPath){
-        helper.removeShellScriptBuildPhase(context, xcodeProjectPath);
-    });
-};
