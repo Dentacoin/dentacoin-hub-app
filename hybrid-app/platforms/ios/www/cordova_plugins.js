7c7a15b4c8d785251ffaee3d1d596d0dc3bf4950
unknown <miroslav.nedelchev@dentacoin.com>
2021-01-11 08:44:27 +0200

    test ios view

--- hybrid-app/platforms/ios/www/cordova_plugins.js
+++ hybrid-app/platforms/ios/www/cordova_plugins.js
@@ -245,6 +245,22 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "Connection"
       ]
+    },
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    },
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "window.WkWebView"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -258,6 +274,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-network-information": "2.0.2",
     "cordova-plugin-whitelist": "1.3.4",
     "cordova-support-android-plugin": "1.0.1",
-    "cordova-support-google-services": "1.3.2"
+    "cordova-support-google-services": "1.3.2",
+    "cordova-plugin-wkwebview-engine": "1.2.1"
   };
 });
