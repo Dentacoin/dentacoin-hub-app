aa8b0627734d92f647aee6270605ea72e33dc846
unknown <miroslav.nedelchev@dentacoin.com>
2021-01-11 10:08:38 +0200

    test apple ios

--- hybrid-app/platforms/ios/platform_www/cordova_plugins.js
+++ hybrid-app/platforms/ios/platform_www/cordova_plugins.js
@@ -231,20 +231,28 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
-      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
-      "pluginId": "cordova-plugin-wkwebview-engine",
-      "clobbers": [
-        "cordova.exec"
-      ]
+      "id": "cordova-plugin-wkwebview-file-xhr.formdata-polyfill",
+      "file": "plugins/cordova-plugin-wkwebview-file-xhr/src/www/ios/formdata-polyfill.js",
+      "pluginId": "cordova-plugin-wkwebview-file-xhr",
+      "runs": true
     },
     {
-      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview",
-      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview.js",
-      "pluginId": "cordova-plugin-wkwebview-engine",
-      "clobbers": [
-        "window.WkWebView"
-      ]
+      "id": "cordova-plugin-wkwebview-file-xhr.xhr-polyfill",
+      "file": "plugins/cordova-plugin-wkwebview-file-xhr/src/www/ios/xhr-polyfill.js",
+      "pluginId": "cordova-plugin-wkwebview-file-xhr",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-wkwebview-file-xhr.fetch-bootstrap",
+      "file": "plugins/cordova-plugin-wkwebview-file-xhr/src/www/ios/fetch-bootstrap.js",
+      "pluginId": "cordova-plugin-wkwebview-file-xhr",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-wkwebview-file-xhr.fetch-polyfill",
+      "file": "plugins/cordova-plugin-wkwebview-file-xhr/src/www/ios/whatwg-fetch-2.0.3.js",
+      "pluginId": "cordova-plugin-wkwebview-file-xhr",
+      "runs": true
     }
   ];
   module.exports.metadata = {
@@ -256,8 +264,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-inappbrowser": "4.0.0",
     "cordova-plugin-network-information": "2.0.2",
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-wkwebview-engine": "1.2.1",
     "cordova-support-android-plugin": "1.0.1",
-    "cordova-support-google-services": "1.3.2"
+    "cordova-support-google-services": "1.3.2",
+    "cordova-plugin-wkwebview-file-xhr": "3.0.0"
   };
 });
