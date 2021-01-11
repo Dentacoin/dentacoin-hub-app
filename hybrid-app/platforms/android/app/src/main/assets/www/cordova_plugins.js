1ba61555ec70e113e13cde42622f991060ae4256
unknown <miroslav.nedelchev@dentacoin.com>
2021-01-11 08:58:29 +0200

    test ios view

--- hybrid-app/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ hybrid-app/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -250,7 +250,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-app-launcher": "0.4.0",
     "cordova-plugin-ionic-webview": "5.0.0",
     "cordova-plugin-inappbrowser": "4.0.0",
-    "cordova-plugin-customurlscheme": "5.0.2",
-    "cordova-plugin-wkwebview-engine": "1.2.1"
+    "cordova-plugin-customurlscheme": "5.0.2"
   };
 });
